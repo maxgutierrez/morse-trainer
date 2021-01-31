3dccb2b6fab07f5f4615f16679092174902f15c9
Max Gutierrez <35828336+maxgutierrez@users.noreply.github.com>
2021-01-31 19:23:40 -0300

    commit pre push

--- main.js
+++ main.js
@@ -1,55 +1,73 @@
-const display = document.querySelector('#display')
-const botoes = document.querySelectorAll('button')
-const alfabeto = [
-  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-]
-const alfabetoMorse = {
-  A:'•-', 
-  B:'-•••', 
-  C:'-•-•', 
-  D:'-••', 
-  E:'•', 
-  F:'••-•',
-  G:'--•', 
-  H:'••••', 
-  I:'••', 
-  J:'•---', 
-  K:'-•-', 
-  L:'•-••',
-  M:'--', 
-  N:'-•', 
-  O:'---', 
-  P:'•--•', 
-  Q:'--•-', 
-  R:'•-•',
-  S:'•••', 
-  T:'-', 
-  U:'••-', 
-  V:'•••-', 
-  W:'•--', 
-  X:'-••-',
-  Y:'-•--',
-  Z:'--••'
-}
+
 function numAleatorioEntre(min, max) {//por padrão vai de 0 até o num designado
   return (parseInt(Math.random() * (max- min + 1)) + min)
 }
-const alfabetoMorseArray = Object.values(alfabetoMorse)
-const numDeLetras = Object.values(alfabetoMorse).length
-const numAleatorio = numAleatorioEntre(0,numDeLetras-1)
-const letraAleatoria = alfabeto[numAleatorio]
-const morseAleatorio = alfabetoMorse[letraAleatoria]
+const display = document.querySelector('#display')
+const botoes = document.querySelectorAll('button')
+const showPontuacao = document.querySelector('#pontuacao')
+const info = document.querySelector('#info')
+const alfabeto = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
+const alfMorse = [
+  "•-", 
+  "-•••", 
+  "-•-•", 
+  "-••", 
+  "•", 
+  "••-•", 
+  "--•", 
+  "••••", 
+  "••", 
+  "•---", 
+  "-•-", 
+  "•-••", 
+  "--", 
+  "-•", 
+  "---", 
+  "•--•", 
+  "--•-", 
+  "•-•", 
+  "•••", 
+  "-", 
+  "••-", 
+  "•••-", 
+  "-••-", 
+  "-•--", 
+  "--••"
+]
 
-function mostrarMorse(){
-  display.innerHTML = morseAleatorio
+const numAleatorio = ()=> numAleatorioEntre(0, showPontuacao.innerHTML)
+const morseAleatorio = ()=> alfMorse[numAleatorio()]
+const mostrarMorse = ()=> display.innerHTML = morseAleatorio()
+const certoOuErrado = (txt, time)=>{
+  info.innerHTML = txt
+  setInterval(() => {
+    info.innerHTML = '-'
+  }, time);
 }
 
-// click no btn
+
+// CLICK NO BOTAO
 botoes.forEach(btn => {
   btn.addEventListener('click', function(){
-    alert(btn.innerHTML)
+
+    const escolhido = alfabeto.indexOf(btn.innerHTML) // index da letra no []
+    const morse = display.innerHTML //morse
+    const pontoAtt = parseInt(showPontuacao.innerHTML)
+
+    if (alfMorse[escolhido] == morse){
+      showPontuacao.innerHTML = pontoAtt + 2
+      certoOuErrado('ACERTOU !', 2500)
+      mostrarMorse()
+
+    } else {
+      showPontuacao.innerHTML = pontoAtt - 2
+      certoOuErrado(`${alfabeto[alfMorse.indexOf(morse)]}`, 2000)
+
+      
+    }
   })
 })
-window.onload(
-  mostrarMorse()
-)
+
+mostrarMorse()
